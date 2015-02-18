a78ecb32ca1ae416177ed4828d4cd8436fe02fc5
Lether1983 <VincentBendigs@googlemail.com>
2015-02-18 13:28:03 +0100

    ADDED: Scene 1 Player Movement and Agent with Lable for States

---
+++ Source/AiTest/Assets/AgentenControllscript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentenControllscript : MonoBehaviour 
+{
+    Vector3 movedirection = Vector3.zero;
+	// Use this for initialization
+
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Transform other = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        if(other)
+        {
+            float dist = Vector3.Distance(this.transform.position, other.transform.position);
+
+            if(dist <= 2.5f && dist > 1f)
+            {
+                this.gameObject.GetComponentInChildren<TextMesh>().text = "Approach";
+            }
+            else if (dist <= 1f)
+            {
+                this.gameObject.GetComponentInChildren<TextMesh>().text = "Attack";
+            }
+            else
+            {
+                this.gameObject.GetComponentInChildren<TextMesh>().text = "Idle";
+            }
+        }
+	}
+}
---
+++ Source/AiTest/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 movedirection = Vector3.zero;
+    float speed = 2f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        CharacterController charController = GetComponent<CharacterController>();
+        Transform other = GameObject.FindGameObjectWithTag("Exit").transform;
+        movedirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        movedirection = transform.TransformDirection(movedirection* speed);
+
+        float dist = Vector3.Distance(other.position, this.transform.position);
+
+        if(dist <= 1)
+        {
+            Application.LoadLevel(1);
+        }
+        charController.Move(movedirection * Time.deltaTime);
+    }
+}
